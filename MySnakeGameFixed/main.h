c19839a9be97e22fd5153faa56a82a431aec8dc1
ParthP22 <134655719+ParthP22@users.noreply.github.com>
2023-07-26 18:54:37 -0400

    Add project files.

---
+++ MySnakeGameFixed/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <string.h>
+
+
+#define LENGTH 600
+#define HEIGHT 600
+#define UNIT_SIZE 25
+
+#endif
