fe286ac3700266ca2ae79f3289a31fd5d5f05f3f
ParthP22 <parthpatel0422@gmail.com>
2025-08-03 17:49:34 -0400

    refactor: added all .h files into the include directory

---
+++ include/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define LENGTH 600
+#define HEIGHT 600
+#define UNIT_SIZE 25
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include "game_state.h"
+#include "game_objects.h"
+
+
+
+
+#endif
